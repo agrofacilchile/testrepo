46da2aa3ddeabfebc1c73ba55587312b72acca34
José Ramírez <131489735+agrofacilchile@users.noreply.github.com>
2023-11-21 17:19:30 -0300

    Add files via upload

---
+++ tipos-de-datos.py
@@ -0,0 +1,23 @@
+"string"
+"esto es un texto"
+'string'
+'Este también es un texto'
+"""string"""
+"""estos a continuación son también textos,
+pero en varias líneas"""
+"""los datos de prueba son:
+clase 1: negro
+Clase 2: blanco
+clase 3: otros"""
+'''string'''
+'''los datos de prueba son:
+clase 1: negro
+Clase 2: blanco
+clase 3: otros'''
+"dato numérico entero - int"
+44
+"dato numerico decimal - float"
+44.5
+"dato boleano: adopta uno de dos valores posibles"
+False
+True
